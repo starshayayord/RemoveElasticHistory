9d115bde99d613c90320f08bc12ef3da372cdfbc
linikova <linikova@skbkontur.ru>
2015-06-05 18:50:30 +0500

    command line args

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 #Documentation
-Delete old indexfiles using elasticsearch api:
+Delete old indexfiles using elasticsearch api.
+
+###Command line args:
+* **-d** or **--days** [int] "days ago" to compartion
+* **-p** or **--port** [int] elasticsearch port
+* **-h** or **--host** [string]elasticsearch host address without http://
+
 ###CRONTAB
 ```sh
 chmod +x /root/Remover.sh
--- Remover.sh
+++ Remover.sh
@@ -1,13 +1,36 @@
 #!/bin/bash
-DAYS=2
-DateToCompare="$(date +%Y%m%d -d $DAYS" days ago")"
-array=( $(curl "http://192.168.76.241:9200/_stats" | grep -o 'logstash\-[[:digit:]]\{4\}\.[[:digit:]]\{2\}\.[[:digit:]]\{2\}') )
+while [[ $# > 0 ]]
+do
+key="$1"
+
+case $key in
+    -d|--days)
+    DAYS="$2"
+    shift # past argument
+    ;;
+    -p|--port)
+    EPORT="$2"
+    shift # past argument
+    ;;
+    -h|--host)
+    EHOST="$2"
+    shift # past argument
+    ;;
+    *)
+     echo "ERROR: Wrong command line parameter"
+     exit 1       # unknown option
+    ;;
+esac
+shift # past argument or value
+done
+DateToCompare="$(date +%Y%m%d -d "${DAYS} days ago")"
+array=( $(curl "http://${EHOST}:${EPORT}/_stats" | grep -o 'logstash\-[[:digit:]]\{4\}\.[[:digit:]]\{2\}\.[[:digit:]]\{2\}') )
 for index in "${array[@]}"
 do
-  indexdate="$(echo $index| grep -o '[[:digit:]]\{4\}\.[[:digit:]]\{2\}\.[[:digit:]]\{2\}' |sed -e 's/\.//g')"
-  if ((DateToCompare > indexdate)) ;  then
+indexdate="$(echo $index| grep -o '[[:digit:]]\{4\}\.[[:digit:]]\{2\}\.[[:digit:]]\{2\}' |sed -e 's/\.//g')"
+if ((DateToCompare > indexdate)) ;  then
     echo "Deleting index $index"
-    curl -X DELETE "http://192.168.76.241:9200/$index"
+    curl -X DELETE "${EHOST}:${EPORT}/$index"
   else
     echo "The $index index is relevant"
   fi
